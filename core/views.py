b15f067f89dc717014c791b0c372b8ffc8882278
Larissa <larissa.carvalhoa57@gmail.com>
2022-08-29 03:31:47 -0300

    lariemii

--- core/views.py
+++ core/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from core.models import Categoria
+from core.serializers import CategoriaSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+from rest_framework.viewsets import ModelViewSet
+
+from core.models import Categoria, Editora
+from core.serializers import CategoriaSerializer, EditoraSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+
+class EditoraViewSet(ModelViewSet):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializer
